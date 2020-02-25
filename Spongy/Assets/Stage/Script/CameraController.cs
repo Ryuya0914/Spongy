ede9c94c076afd62a0d375dd23be564edd8fa161
tanaka0914 <tanaka.ryuya.0914@gmail.com>
2020-02-25 13:48:41 +0900

    ステージ手直し

---
+++ Spongy/Assets/Stage/Script/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    float speed = 0.5f;                     // カメラが動く最大速度
+    Vector3 vec = new Vector3(0, 0, 0);
+
+    void Update()
+    {
+        // カメラの中心にプレイヤーが来るように移動する
+        vec = player.transform.position - transform.position;                   // 位置の差分
+        vec.x = (vec.x > speed) ? speed : ((vec.x < -speed) ? -speed : vec.x);  // xの値がspeedを超えないようにする
+        vec.y = (vec.y > speed) ? speed : ((vec.y < -speed) ? -speed : vec.y);  // yの値がspeedを超えないようにする
+        vec.z = 0f;
+        transform.position += vec;
+    }
+}
