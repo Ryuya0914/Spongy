d526ed58f84d1f33f3d8112b0e4b4263816cee61
tanaka0914 <tanaka.ryuya.0914@gmail.com>
2020-02-28 12:04:41 +0900

    ゲームクリア追加

---
+++ Spongy/Assets/Stage/Script/Clear.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clear : MonoBehaviour
+{
+    [SerializeField] GameObject ui;
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.tag == "Player") {
+            ui.SetActive(true);
+        }
+    }
+
+}
---
+++ Spongy/Assets/Stage/Script/SceneMane.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMane : MonoBehaviour
+{
+    [SerializeField] string nextSceneName;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return)) {
+            Invoke("SceneLoad", 1f);
+        }
+    }
+
+    void SceneLoad() {
+        SceneManager.LoadScene(nextSceneName);
+    }
+}
