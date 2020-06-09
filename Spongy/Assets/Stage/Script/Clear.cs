adc184332fafb1e6786fa9fc504c76315edfa079
tanaka0914 <tanaka.ryuya.0914@gmail.com>
2020-06-09 14:17:12 +0900

    UIの見た目いじって、クリア時にタイムを表示する

--- Spongy/Assets/Stage/Script/Clear.cs
+++ Spongy/Assets/Stage/Script/Clear.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Clear : MonoBehaviour
 {
     [SerializeField] GameObject ui;
+    [SerializeField] Text _text;
+    [SerializeField] TimeCounter timecounter;
 
     void OnTriggerEnter2D(Collider2D col) {
         if (col.tag == "Player") {
             ui.SetActive(true);
+            _text.text = timecounter.GetTime.ToString("000.00");   // 画面に時間を表示
         }
     }
 
