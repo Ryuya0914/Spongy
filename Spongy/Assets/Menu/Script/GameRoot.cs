69d617e9af442c9311b860e6b196b150f79e9d62
tanaka0914 <tanaka.ryuya.0914@gmail.com>
2020-06-10 14:57:50 +0900

    カメラの位置とか変えた

--- Spongy/Assets/Menu/Script/GameRoot.cs
+++ Spongy/Assets/Menu/Script/GameRoot.cs
@@ -9,11 +9,14 @@ public class GameRoot : MonoBehaviour {
     // 0 = title , 1 = game, 2 = clear
     static public int State = 0;
     [SerializeField] Button btn;
+    [SerializeField] GameObject obj_canvas;
+    
     
     void Start() {
-        if (State != 0)
-        btn.onClick.Invoke();
+        if(State != 0)
+            btn.onClick.Invoke();
+        else
+            obj_canvas.SetActive(true);
     }
-    
 
 }
--- Spongy/Assets/Stage/Script/CameraController.cs
+++ Spongy/Assets/Stage/Script/CameraController.cs
@@ -6,14 +6,16 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] GameObject player;
     float speed = 0.8f;                     // カメラが動く最大速度
-    float H = 6.0f;                         // カメラの縦の長さ
-    float W = 10.0f;                         // カメラの横の長さ
+    float H = 9.0f;                         // カメラの縦の長さ
+    float W = 16.0f;                         // カメラの横の長さ
+    float Hup = 0.8f;                       // カメラの位置
     Vector3 vec = new Vector3(0, 0, 0);
 
     void Update()
     {
         // カメラの中心にプレイヤーが来るように移動する
         vec = player.transform.position - transform.position;                   // 位置の差分
+        vec.y += Hup;   // カメラの位置をプレイヤーより少し上にする
         vec.x = (vec.x > W) ? vec.x - W : ((vec.x < -W) ? vec.x + W : ((vec.x > speed) ? speed : ((vec.x < -speed) ? -speed : vec.x)));
         vec.y = (vec.y > H) ? vec.y - H : ((vec.y < -H) ? vec.y + H : ((vec.y > speed) ? speed : ((vec.y < -speed) ? -speed : vec.y)));
         //vec.x = ((vec.x > speed) ? speed : ((vec.x < -speed) ? -speed : vec.x));  // xの値がspeedを超えないようにする
